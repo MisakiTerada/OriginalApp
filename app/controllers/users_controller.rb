e6305997a6a3eed929a11816a63f9f4c08abc069
EC2 Default User <ec2-user@ip-172-31-80-53.ec2.internal>
2019-03-10 10:22:57 +0000

    originalapp2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
